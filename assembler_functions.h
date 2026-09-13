01402cb5ad4dd3ae8b48441470981abfdcd3bba2
Hugo Torres Tenreiro <hugotorrestenreiro@gmail.com>
2026-09-13 18:49:51 +0200

    Jump and Calls functions fix

--- assembler_functions.h
+++ assembler_functions.h
@@ -12,4 +12,9 @@ uint8_t find_KK(uint16_t value);
 uint16_t find_NNN(uint16_t value);
 */
 
+void JP_NNN (chip8 *cpu, uint16_t NNN);
+void CALL_NNN (chip8 *cpu , uint16_t NNN);
+void RET (chip8 *cpu);
+void JP_V0_NNN (chip8 *cpu, uint16_t NNN);
+
 #endif
